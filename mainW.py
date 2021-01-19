f9591927d799841467b3d052df288b6a78d6b0ad
Ivanjtm <ivanjtm@gmail.com>
2021-01-18 21:05:25 -0600

    Code working for Windows

---
+++ mainW.py
@@ -0,0 +1,53 @@
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+import time
+import requests
+
+print('Running')
+
+
+
+def writeDoc(name, text):
+    path = 'C:/Users/ivanj/Desktop/Output/' + name
+    f = open(path, "a")
+    f.truncate(0)
+    f.write(text)
+    f.close()
+
+def downloadPhoto(link):
+    image_url = link.replace('https://youtu.be/','https://img.youtube.com/vi/')
+    image_url = image_url + '/0.jpg'
+    print(image_url)
+    img_data = requests.get(image_url).content
+    open('C:/Users/ivanj/Desktop/Output/image.jpg', 'wb').truncate(0)
+    with open('C:/Users/ivanj/Desktop/Output/image.jpg', 'wb') as handler:
+        handler.write(img_data)
+
+class Handler (FileSystemEventHandler):
+    def on_modified(self, event):
+    
+        data = open(inputFile, "r", encoding='utf-8').read().split('|')
+        
+        if data != ['']:
+            print(data)
+            writeDoc('title.txt', data[0])
+            writeDoc('duration.txt', data[1])
+            downloadPhoto(data[2])
+            open(inputFile).close()
+        
+
+inputFolder = 'C:/Users/ivanj/Documents/Nightbot'
+inputFile = inputFolder + '/' + 'current_song.txt'
+
+event_handler = Handler()
+observer = Observer()
+observer.schedule(event_handler, inputFolder, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
